af565489aeaa75062f8754a99396b921fa16dfe1
Tenille <elmofreak@gmail.com>
2025-01-14 20:09:14 +0800

    add workshops model

--- postitivity/workshops/models.py
+++ postitivity/workshops/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Workshop(models.Model):
+   title = models.CharField(max_length=200)
+   start_date = models.DateTimeField()
+   end_date = models.DateTimeField()
+   category = models.IntegerField()
+   image_url = models.URLField()
+   date_created = models.DateTimeField(auto_now_add=True)
+   created_by_user = models.IntegerField()
+   location = models.IntegerField(null=True, blank=True)
+   coding_language = models.IntegerField(null=True, blank=True)
+   organisation = models.IntegerField(null=True, blank=True)
+   is_archived = models.BooleanField()
+   archive_id = models.IntegerField(null=True, blank=True)
+
