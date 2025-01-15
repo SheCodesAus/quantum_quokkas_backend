b6950a9ef2d2bfa58b25eaf7767f8d1ff950b24c
Tenille Scott <tenille.scott@riotinto.com>
2025-01-15 12:01:50 +0800

    add remaining tables to models (user FK commented out)

--- postitivity/workshops/models.py
+++ postitivity/workshops/models.py
@@ -1,40 +1,156 @@
 from django.db import models
 
 # Create your models here.
-class Workshop(models.Model):
-   title = models.CharField(max_length=200)
-   start_date = models.DateTimeField()
-   end_date = models.DateTimeField()
-   category = models.IntegerField(null=True, blank=True)
-   image_url = models.URLField()
-   date_created = models.DateTimeField(auto_now_add=True)
-   created_by_user = models.IntegerField()
-   location = models.IntegerField(null=True, blank=True)
-   coding_language = models.IntegerField(null=True, blank=True)
-   organisation = models.IntegerField(null=True, blank=True)
-   is_archived = models.BooleanField(default=0)
-   archive_id = models.IntegerField(null=True, blank=True)
-
-class Notes(models.Model):
-   content = models.CharField(max_length=200)
-   user_id = models.IntegerField()
-   anonymous = models.BooleanField()
-   date_created = models.DateTimeField(auto_now_add=True)
-   added_by_user = models.IntegerField()
-   note_category = models.IntegerField(null=True, blank=True)
-   coding_language = models.IntegerField(null=True, blank=True)
-   likes_count = models.IntegerField(default=0)
-   is_archived = models.BooleanField(default=0)
-   archive_id = models.IntegerField(null=True, blank=True)
 
 class Location(models.Model):
    location = models.CharField()
    created_date = models.DateTimeField(auto_now_add=True)
    added_by_user = models.IntegerField()
-   is_archived = models.BooleanField(default=0)
+      # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='created_locations'
+   #  )
+   is_archived = models.BooleanField()
 
 class Cohorts(models.Model):
    cohort_name = models.CharField()
    added_by_user = models.IntegerField()
+      # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='created_cohorts'
+   #  )
    created_date = models.DateTimeField(auto_now_add=True)
-   is_archived = models.BooleanField(default=0)
+   is_archived = models.BooleanField()
+
+class Archive_details(models.Model):
+   archive_table = models.CharField()
+   archive_date = models.DateTimeField(auto_now_add=True)
+   archive_user = models.IntegerField()
+      # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='archived_items'
+   #  )
+   archive_reason = models.CharField()
+
+class Note_category(models.Model):
+   note_category_name = models.CharField()
+   added_date = models.DateTimeField(auto_now_add=True)
+   added_by_user = models.IntegerField()
+      # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='created_note_categories'
+   #  )
+   is_archived = models.BooleanField()
+
+ class Category(models.Model):
+   category_name = models.CharField()
+   added_date = models.DateTimeField(auto_now_add=True)
+   added_by_user = models.IntegerField()
+      # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='created_categories'
+   #  )
+   is_archived = models.BooleanField()  
+
+class Coding_language(models.Model):
+   language = models.CharField()
+   added_date = models.DateTimeField(auto_now_add=True)
+   added_by_user = models.IntegerField()
+      # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='created_languages'
+   #  )
+   is_archived = models.BooleanField()  
+
+
+class Organisation(models.Model):
+   organisation_name = models.CharField()
+   added_date = models.DateTimeField(auto_now_add=True)
+   added_by_user = models.IntegerField()
+      # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='added_organisation'
+   #  )
+   is_archived = models.BooleanField()     
+
+class Workshop(models.Model):
+   title = models.CharField(max_length=200)
+   start_date = models.DateTimeField()
+   end_date = models.DateTimeField()
+   image_url = models.URLField(max_length=2000)
+   date_created = models.DateTimeField(auto_now_add=True)
+   created_by_user = models.IntegerField()
+   # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='created_workshops'
+   #  )
+   location = models.ForeignKey(
+        'Location',
+         on_delete=models.CASCADE,
+         related_name='category_workshops',
+         null=True, blank=True)
+   category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        related_name='workshops',
+        null=True, blank=True
+    )      
+   coding_language = models.ForeignKey(
+        'Coding_language',
+         on_delete=models.CASCADE,
+         related_name='language_workshops',
+         null=True, blank=True)
+   organisation = models.ForeignKey(
+        'Organisation',
+         on_delete=models.CASCADE,
+         related_name='organisation_workshops',
+         null=True, blank=True)
+   is_archived = models.BooleanField()
+   archive_id = models.ForeignKey(
+        'Archive_details',
+         on_delete=models.CASCADE,
+         related_name='archived_workshops',
+         null=True, blank=True)
+
+class Notes(models.Model):
+   content = models.CharField(max_length=200)
+   user_id = models.IntegerField()
+   # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='created_notes'
+   #  )
+   anonymous = models.BooleanField()
+   date_created = models.DateTimeField(auto_now_add=True)
+   added_by_user = models.IntegerField()
+   # models.ForeignKey(
+   #      get_user_model(),
+   #      on_delete=models.CASCADE,
+   #      related_name='added_notes'
+   #  )
+   note_category = models.ForeignKey(
+        'Note_category',
+        on_delete=models.CASCADE,
+        related_name='notes',
+        null=True, blank=True
+    )      
+   coding_language = models.ForeignKey(
+        'Coding_language',
+         on_delete=models.CASCADE,
+         related_name='language_notes',
+         null=True, blank=True)
+   likes_count = models.IntegerField()
+   is_archived = models.BooleanField()
+   archive_id = models.ForeignKey(
+        'Archive_details',
+         on_delete=models.CASCADE,
+         related_name='archived_notes',
+         null=True, blank=True)
