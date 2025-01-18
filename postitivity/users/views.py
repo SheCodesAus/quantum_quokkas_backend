017cd0034bd03e4295cb8ecdb9705f790d9acd18
lauschof <schofieldlaura@outlook.com>
2025-01-18 17:17:11 +0800

    updated user views

--- postitivity/users/views.py
+++ postitivity/users/views.py
@@ -1,14 +1,13 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from .serializers import CustomUserSerializer
 
-# Create your views here.
+#View all users or create a new user
 
-#Get all users or create a new user
-
-class UserList(APIView):
+class CustomUserList(APIView):
 
     def get(self, request):
         User = get_user_model()
@@ -28,3 +27,31 @@ class UserList(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+# View  for viewing and updating a specific user's details
+
+class CustomUserDetail(APIView):
+    def get_object(self, pk):
+        User = get_user_model()
+        try:
+            return User.objects.get(pk=pk)    
+        except User.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data)
+    
+    def patch(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+
+
+
+        
