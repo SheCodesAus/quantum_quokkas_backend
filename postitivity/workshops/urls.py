0c0d7ac96368979ce172222ff75f1fdbe04889f2
Tenille <elmofreak@gmail.com>
2025-01-21 08:35:45 +0800

    adding token auth url as the rest seems set up

--- postitivity/workshops/urls.py
+++ postitivity/workshops/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from users.views import CustomAuthToken
 
 urlpatterns = [
     path('workshops/', views.WorkshopList.as_view()),
     path('workshops/<int:pk>/', views.WorkshopDetail.as_view()),
     path('notes/', views.Notelist.as_view()),
-   
+    path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),  
 ]
